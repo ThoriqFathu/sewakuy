9f378dacd6fb77771af78f3dd9c72d6dfe869ce1
ThoriqFathu <thoriq771@gmail.com>
2023-05-04 12:28:55 +0700

    Tambah Layout publik

---
+++ app/Views/layout/templete-public.php
@@ -0,0 +1,97 @@
+<!DOCTYPE html>
+<html lang="en">
+
+
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+  <title>Sewa Kuy</title>
+  <meta content="" name="description">
+  <meta content="" name="keywords">
+
+  <!-- Favicons -->
+  <link href="<?= base_url() ?>assets/img/favicon.png" rel="icon">
+  <link href="<?= base_url() ?>assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+
+  <!-- Google Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Roboto:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+  <!-- jQuery Rating -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/rateYo/2.3.2/jquery.rateyo.min.css">
+
+  <!-- Vendor CSS Files -->
+  <link href="<?= base_url() ?>assets/vendor/aos/aos.css" rel="stylesheet">
+  <link href="<?= base_url() ?>assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="<?= base_url() ?>assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+  <link href="<?= base_url() ?>assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+  <link href="<?= base_url() ?>assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+
+  <!-- Template Main CSS File -->
+  <link href="<?= base_url() ?>assets/css/style.css" rel="stylesheet">
+
+  <!-- =======================================================
+  * Template Name: SoftLand - v4.7.0
+  * Templatebase_url(): https://bootstrapmade.com/softland-bootstrap-app-landing-page-template/
+  * Author: BootstrapMade.com
+  * License: https://bootstrapmade.com/license/
+  ======================================================== -->
+</head>
+
+<body>
+  <!-- ======= Header ======= -->
+  <header id="header" class="fixed-top d-flex align-items-center header-scrolled">
+    <div class="container d-flex justify-content-between align-items-center">
+
+      <div class="logo">
+        <h1><a href="#">SEWAKUY</a></h1>
+        <!-- Uncomment below if you prefer to use an image logo -->
+        <!-- <a href="index.html"><img src="<?= base_url() ?>assets/img/logo.png" alt="" class="img-fluid"></a>-->
+      </div>
+
+      <nav id="navbar" class="navbar">
+        <ul>
+          <li><a href=<?php echo base_url("/") ?>>Beranda</a></li>
+          <li><a href=<?php echo base_url() . '/#/' ?>>Lapangan</a></li>
+          <li><a href=<?php echo base_url() . "/#/" ?>>Login</a></li>
+          <li><a href=<?php echo base_url() . "/#/" ?>>Register</a></li>
+        </ul>
+        <i class="bi bi-list mobile-nav-toggle"></i>
+      </nav><!-- .navbar -->
+    </div>
+  </header><!-- End Header -->
+
+  <?= $this->renderSection('content') ?>
+
+
+  <!-- ======= Footer ======= -->
+  <footer class="footer" role="contentinfo">
+
+    <div class="row justify-content-center text-center">
+      <div class="col-md-7">
+        <p class="copyright">&copy; Copyright Sewakuy. All Rights Reserved</p>
+        <div class="credits">
+
+          Designed by Kelompok 1 Framework
+        </div>
+      </div>
+    </div>
+
+    </div>
+  </footer>
+
+  <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+
+  <!-- Vendor JS Files -->
+  <script src="<?= base_url() ?>assets/vendor/aos/aos.js"></script>
+  <script src="<?= base_url() ?>assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="<?= base_url() ?>assets/vendor/swiper/swiper-bundle.min.js"></script>
+  <script src="<?= base_url() ?>assets/vendor/php-email-form/validate.js"></script>
+
+  <!-- Template Main JS File -->
+  <script src="<?= base_url() ?>assets/js/main.js"></script>
+
+</body>
+
+
+
+</html>
